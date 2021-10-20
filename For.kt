d142c42dd0ea710f9daf8c62e89bb48c0631f3d9
Astrea08 <92804437+Astrea08@users.noreply.github.com>
2021-10-20 08:48:41 +0700

    Kotlin-
    For With kotlin

---
+++ For.kt
@@ -0,0 +1,9 @@
+fun main() {
+    for (value in 100 downTo 0 step 5)
+        println(value)
+    for (pablo in 'z' downTo 'a' step 5)
+        println(pablo)
+val name = "Dimas"
+    for (name in name)
+        println("\"$name\"")
+}
